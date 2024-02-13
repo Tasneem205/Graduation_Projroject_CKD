d2e27e4fa3592f04adfe605709803e19878ae889
Tasneem Abdeltawab <tasneemabdeltawab205@gmail.com>
2024-02-13 05:24:56 +0200

    initial commit in doctor_backend

---
+++ doctor_backend/main.js
@@ -0,0 +1,23 @@
+import Express from "express";
+// import router from "./src/controllers/index.controller.js";
+import dotenv from "dotenv";
+// import errorHandler from "./src/middleWares/errorHandler.js";
+import morgan from "morgan";
+
+//.format('YYYY-MM-DD').utc()
+
+const app = Express();
+
+dotenv.config();
+
+app.use(Express.json());
+
+app.use(morgan("dev"));
+
+// app.use("/clinic", router);
+
+// app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port 4000");
+});
