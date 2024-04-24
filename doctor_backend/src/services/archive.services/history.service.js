148b17189314a571c50eeacec6d6eaeb99e2b5df
Tasneem Abdeltawab <tasneemabdeltawab205@gmail.com>
2024-04-24 19:33:44 +0200

    get history service done

--- doctor_backend/src/services/archive.services/history.service.js
+++ doctor_backend/src/services/archive.services/history.service.js
@@ -1,8 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import responses from "../../helpers/responses.js";
+import {all} from "express/lib/application.js";
 
+const prisma = new PrismaClient();
 const getHistory = async (req, res, next) => {
-    // TODO: implement me
+    const allData = await prisma.reservations.findMany();
+    let data = {"reservation": [], "first": [], "followUp": []};
+    for (const ele of allData) {
+        if (ele.type === "First") {
+            data.first.push(ele);
+        } else if (ele.type === "Reservation") {
+            data.reservation.push(ele);
+        } else if (ele.type === "FollowUP") {
+            data.followUp.push(ele);
+        }
+    }
 };
 
 export default getHistory;
