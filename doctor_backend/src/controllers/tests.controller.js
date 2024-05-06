c7699ded44d81f0bac5aa6ae2d5f84065c32d63a
Tasneem Abdeltawab <tasneemabdeltawab205@gmail.com>
2024-05-06 19:44:52 +0300

    prisma file edit

--- doctor_backend/src/controllers/tests.controller.js
+++ doctor_backend/src/controllers/tests.controller.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import postFunctions from "../services/tests.services/post.service.js";
 
 const testRouter = new Router();
+
+testRouter.post("/", postFunctions.runModel());
 
 export default testRouter;
